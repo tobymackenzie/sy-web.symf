dec88e5c9e822260a9ddae021fa86abd9fed902e
Toby Mackenzie <public@tobymackenzie.com>
2019-07-06 22:30:22 -0400

    kernel: rename variable to make less confusing, shorter

--- Component/AppKernel.php
+++ Component/AppKernel.php
@@ -5,40 +5,40 @@ use Symfony\Component\Config\Loader\LoaderInterface;
 use Symfony\Component\HttpKernel\Kernel;
 use TJM\Bundle\StandardEditionBundle\Component\App\App;
 class AppKernel extends Kernel{
-	protected $appContainer;
-	public function __construct($appContainerOrEnv = null, $debug = null){
-		if(!$appContainerOrEnv){
-			$appContainerOrEnv = App::getSingleton();
-		}elseif(is_string($appContainerOrEnv)){
+	protected $app;
+	public function __construct($appOrEnv = null, $debug = null){
+		if(!$appOrEnv){
+			$appOrEnv = App::getSingleton();
+		}elseif(is_string($appOrEnv)){
 			//--support symfony kernel interface.  We need a better way to go about this.  This is a stopgap
-			$env = $appContainerOrEnv;
-			$appContainerOrEnv = App::getSingleton();
-			$appContainerOrEnv->setEnvironment($env);
+			$env = $appOrEnv;
+			$appOrEnv = App::getSingleton();
+			$appOrEnv->setEnvironment($env);
 			if($debug){
-				$appContainerOrEnv->setDebug($debug);
+				$appOrEnv->setDebug($debug);
 			}
 		}
-		$this->appContainer = $appContainerOrEnv;
-		parent::__construct($appContainerOrEnv->getEnvironment(), $appContainerOrEnv->getDebug());
+		$this->app = $appOrEnv;
+		parent::__construct($appOrEnv->getEnvironment(), $appOrEnv->getDebug());
 	}
 	public function getCacheDir(){
-		return $this->appContainer->getPath('cache.' . $this->getEnvironment());
+		return $this->app->getPath('cache.' . $this->getEnvironment());
 	}
 	protected function getKernelParameters(){
 		$params = parent::getKernelParameters();
-		$params['tjm.kernel.config_dir'] = $this->appContainer->getPath('config');
-		$params['tjm.kernel.var_dir'] = $this->appContainer->getPath('var');
+		$params['tjm.kernel.config_dir'] = $this->app->getPath('config');
+		$params['tjm.kernel.var_dir'] = $this->app->getPath('var');
 		return $params;
 	}
 	public function getLogDir(){
-		return $this->appContainer->getPath('logs');
+		return $this->app->getPath('logs');
 	}
 	public function getProjectDir(){
-		return $this->appContainer->getPath('project');
+		return $this->app->getPath('project');
 	}
 	public function getRootDir(){
 		if(!isset($this->rootDir)){
-			$this->rootDir = $this->appContainer->getPath('app');
+			$this->rootDir = $this->app->getPath('app');
 		}
 		return $this->rootDir;
 	}
@@ -47,10 +47,10 @@ class AppKernel extends Kernel{
 	==initialization
 	=====*/
 	public function registerBundles(){
-		return $this->appContainer->initBundles();
+		return $this->app->initBundles();
 	}
 
 	public function registerContainerConfiguration(LoaderInterface $loader){
-		$loader->load($this->appContainer->getConfigPath($this->getEnvironment()));
+		$loader->load($this->app->getConfigPath($this->getEnvironment()));
 	}
 }
